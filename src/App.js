a6fb8515e7d548df971f2cbfe3ae28258f5ae9e3
Sami Ullah <38462934+SamiUllah365@users.noreply.github.com>
2023-08-19 21:25:18 +0500

    App.js update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import './App.css';
 import MainMenuPage from './Page/MainPage';
 
 function App() {
   return (
     <>
-    <MainMenuPage/>
+    <BrowserRouter>
+    <Routes>
+      <Route exact path='/' component={<MainMenuPage/>}/>
+    </Routes>
+    </BrowserRouter>    
     </>
       );
 }
